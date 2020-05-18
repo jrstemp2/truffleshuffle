874aa2cf8de9f9ce1e20f9c9a148e7fdc7bea709
Shelton <dellashelton@quickenloans.com>
2020-05-18 11:35:44 -0400

    user model added

---
+++ TruffleShuffle/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruffleShuffle.Models
+{
+    public class Users
+    {
+
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassword { get; set; }
+        public string DisplayName { get; set; }
+        public int WeightLossGoal { get; set; }
+    }
+}
