b307ea58059a6b18d89f4c855048284ca2490fbb
Schwartz2 (Secondary) <MSchwartz2-@quickenloans.com>
2020-05-18 11:41:31 -0400

    weight model

---
+++ TruffleShuffle/Models/weights.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruffleShuffle.Models
+{
+    public class Weights
+    {
+
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public DateTime WeighInDate { get; set; }
+        public int CurrentWeight { get; set; }
+    }
+}
