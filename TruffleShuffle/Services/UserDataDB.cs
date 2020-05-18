1d786d4d0d3731e6229fa2a1d1938db759d30206
Della Shelton <DShelton-@quickenloans.com>
2020-05-18 14:39:53 -0400

    UserDataDB updated

--- TruffleShuffle/Services/UserDataDB.cs
+++ TruffleShuffle/Services/UserDataDB.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using TruffleShuffle.Models;
@@ -8,34 +11,102 @@ namespace TruffleShuffle.Services
 {
     public class UserDataDB : IUserData
     {
+        private readonly string connstring;
+        public UserDataDB(IConfiguration config)
+        {
+            connstring = config.GetConnectionString("default");
+        }
+
         public User AddUser(User user)
         {
-            throw new NotImplementedException();
+            string queryString = "INSERT INTO Users(UserID, UserPassWord,DisplayName,WeightLossGoal) " +
+                "OUTPUT Inserted.UserID, Inserted.UserPassword, Inserted.DisplayName,Inserted.WeightLossGoal " +
+                "VAlUES(@UserID,@UserPassword,@Displayname,@WeightLossGoal) ";
+
+
+            User result = null;
+           
+            using(var conn = new SqlConnection(connstring))
+            {
+                result = conn.QueryFirstOrDefault<User>(queryString, user);
+            }
+
+            return result;
         }
 
         public int DeleteUserByID(int id)
         {
-            throw new NotImplementedException();
+            string queryString = "DELETE FROM Users WHERE ID = @id";
+
+            int result = 0;
+
+            using (var conn = new SqlConnection(connstring))
+            {
+                result = conn.Execute(queryString, new { id = id });
+            }
+
+            return result;
         }
 
         public User GetUserByID(int id)
         {
-            throw new NotImplementedException();
+            string queryString = "Select DisplayName, WeightLossGoal FROM Users WHERE ID=@id";
+
+            User result = null;
+
+            using (var conn = new SqlConnection(connstring))
+            {
+                result = conn.QueryFirstOrDefault<User>(queryString, new { id = id });
+            }
+
+            return result;
         }
 
         public User GetUserByUserName(string username)
         {
-            throw new NotImplementedException();
+            string queryString = "Select DisplayName, WeightLossGoal FROM Users WHERE UserName Like @username";
+
+            User result = null;
+
+            using (var conn = new SqlConnection(connstring))
+            {
+                result = conn.QueryFirstOrDefault<User>(queryString, new { username = username });
+            }
+
+            return result;
         }
 
-        public int UpdateDisplayName(int UserID, string displayName)
+        public int UpdateDisplayName(int userID, string displayName)
         {
-            throw new NotImplementedException();
+            string queryString = "UPDATE Users " +
+                "SET DisplayName = @DisplayName " +
+                "Where ID=@id";
+
+            int result = 0;
+
+            using (var conn = new SqlConnection(connstring))
+            {
+                result = conn.Execute(queryString, new { id = userID, displayName = displayName });
+            }
+
+            return result;
         }
 
-        public int UpdateWeightLossGoal(int UserID, int weightLossGoal)
+        public int UpdateWeightLossGoal(int userID, int weightLossGoal)
         {
-            throw new NotImplementedException();
+
+            string queryString = "UPDATE Users " +
+                "SET WeightLossGoal = @WeightLossGoal " +
+                "Where ID=@id";
+
+            int result = 0;
+
+            using (var conn = new SqlConnection(connstring))
+            {
+                result = conn.Execute(queryString, new { id = userID, weightLossGoal = weightLossGoal });
+            }
+
+            return result;
         }
     }
 }
