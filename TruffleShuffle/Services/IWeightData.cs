feb8ec1efc32d4a16b995840af6f5b2b36da2424
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-05-18 11:55:39 -0400

    Added Weight Data DAL

---
+++ TruffleShuffle/Services/IWeightData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TruffleShuffle.Models;
+
+namespace TruffleShuffle.Services
+{
+    interface IWeightData
+    {
+        public IEnumerable<WeightRecord> GetWeightRecordsByUserID(int ID);
+    }
+}
