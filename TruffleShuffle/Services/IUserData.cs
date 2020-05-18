ab96bfeb303937d7dff26f4ed5eb6a5ee18fbca9
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-05-18 12:08:20 -0400

    Added CRUD interface to User DAL

--- TruffleShuffle/Services/IUserData.cs
+++ TruffleShuffle/Services/IUserData.cs
@@ -10,7 +10,16 @@ namespace TruffleShuffle.Services
 {
     interface IUserData
     {
+        // create
+        public User AddUser(User user);
+        // update
+        public int UpdateWeightLossGoal(int UserID, int weightLossGoal);
+        public int UpdateDisplayName(int UserID, string displayName);
+        // read
         public User GetUserByID(int id);
+        public User GetUserByUserName(string username);
+        // delete
+        public int DeleteUserByID(int id);
 
 
     }
--- TruffleShuffle/Services/UserDataDB.cs
+++ TruffleShuffle/Services/UserDataDB.cs
@@ -8,7 +8,32 @@ namespace TruffleShuffle.Services
 {
     public class UserDataDB : IUserData
     {
+        public User AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteUserByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
         public User GetUserByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUserByUserName(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateDisplayName(int UserID, string displayName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateWeightLossGoal(int UserID, int weightLossGoal)
         {
             throw new NotImplementedException();
         }
