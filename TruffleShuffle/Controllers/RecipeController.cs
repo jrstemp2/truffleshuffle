8e1ca74a66c3648ec87d1820b0d3c3a91b6d2b18
John Stempowski <johnstempowski@quickenloans.com>
2020-05-19 14:21:51 -0400

    Updated Recipe Info

---
+++ TruffleShuffle/Controllers/RecipeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TruffleShuffle.Models;
+using TruffleShuffle.Services;
+using Microsoft.Extensions.Configuration;
+
+namespace TruffleShuffle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly IRecipeData recipeData;
+
+        public RecipeController(IRecipeData recipeData)
+        {
+            this.recipeData = recipeData;
+        }
+
+
+        [HttpPost]
+        public int AddRecipe(Recipe recipe)
+        {
+            return recipeData.AddRecipe(recipe);
+        }
+
+        [HttpGet]
+        public IEnumerable<Recipe> GetAllRecipes()
+        {
+            return recipeData.GetAllRecipes();
+        }
+    }
+}
