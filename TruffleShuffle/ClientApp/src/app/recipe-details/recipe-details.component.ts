923ec260e2c31db8c34a883f355b475a718dbafd
John Stempowski <johnstempowski@quickenloans.com>
2020-05-26 15:09:26 -0400

    now able to add favorites for unique userID's

--- TruffleShuffle/ClientApp/src/app/recipe-details/recipe-details.component.ts
+++ TruffleShuffle/ClientApp/src/app/recipe-details/recipe-details.component.ts
@@ -3,8 +3,9 @@ import { RecipeService } from '../services/recipe.service';
 import { RecipeFavorite } from '../interfaces/recipefavorite';
 import { RecipeFavoriteService } from '../services/recipe-favorite.service';
 import { JoinedRF } from '../interfaces/joinedRF';
-import { User } from '../interfaces/user';
 
+import { User } from '../interfaces/user';
+import { UserService } from '../services/user.service';
 
 import { Recipe } from '../interfaces/recipe';
 import { ActivatedRoute } from '@angular/router';
@@ -18,34 +19,48 @@ import { ActivatedRoute } from '@angular/router';
 /** recipeDetails component*/
 export class RecipeDetailsComponent {
     /** recipeDetails ctor */
-  constructor(private recipeData: RecipeService, private recipeFavoriteData: RecipeFavoriteService, private route: ActivatedRoute) { }
+  constructor(private recipeData: RecipeService, private recipeFavoriteData: RecipeFavoriteService, private userData: UserService, private route: ActivatedRoute) { }
   @Input() ref: string;
   recipe: Recipe;
-  id: number;
+  recipeID: number;
+
+  user: User;
 
   favRecipes: JoinedRF[];
 
   loadPage() {
+
+    this.user = this.userData.getUser();
+
     this.addFav = false;
     this.includes = false;
 
     this.route.params.subscribe(params => {
-      this.id = +params['id'];
-      this.recipeData.getRecipeByID(this.id).subscribe(
+      this.recipeID = +params['id'];
+      this.recipeData.getRecipeByID(this.recipeID).subscribe(
         (data: Recipe) => this.recipe = { ...data },
         error => console.error(error)
       );
     })
-    //---------------Favorites-------------------------------
-    this.route.params.subscribe(params => {
-      this.id = 1;
 
-      console.log("Attempting to retrieve data")
-      this.recipeFavoriteData.getFavoritesByUserID(this.id).subscribe(
-        (data: JoinedRF[]) => this.favRecipes = data,
-        error => console.error(error)
-      );
-    })
+    //---------------Favorites-------------------------------
+
+    //this.route.params.subscribe(params => {
+    //  this.id = +params['id']       ???this.user.id;
+
+    //  console.log("Attempting to retrieve data")
+    //  this.recipeFavoriteData.getFavoritesByUserID(this.user.id).subscribe(
+    //    (data: JoinedRF[]) => this.favRecipes = data,
+    //    error => console.error(error)
+    //  );
+    //})
+
+    this.recipeFavoriteData.getFavoritesByUserID(this.user.id).subscribe(
+      (data: JoinedRF[]) => this.favRecipes = data,
+      error => console.error(error)
+    );
+   
+
   }
 
   ngOnInit() {
@@ -53,77 +68,66 @@ export class RecipeDetailsComponent {
   }
 
   //Add Fav
+
   addFav: boolean = false;
+  includes: boolean = false;
+  addtoFav: boolean = true;
 
   addToFavorite(id: number) {
     this.putFavInArray(id);
     this.addFav = true;
   }
 
-  includes: boolean = false;
-  addtoFav: boolean = true;
-
   checkDups(id: number) {
+
     console.log(this.favRecipes)
     for (let i = 0; i < this.favRecipes.length; i++) {
       console.log('recipeID' + this.favRecipes[i].recipeID);
+
       if (this.favRecipes[i].recipeID === id) {
           this.includes = true;
           this.addtoFav = false;
           break;
       }
     }
-
     if (this.addtoFav === true) {
       this.addToFavorite(id);
-      
     }
   }
   
 
-  putFavInArray(id:number) {
-    this.recipeFavoriteData.addToFavorite(id).subscribe(
+  putFavInArray(id: number) {
+    this.recipeFavoriteData.addToFavorite(id, this.user.id).subscribe(
       (data: any) => {
         console.log('success! ' + id)
-        this.recipeFavoriteData.getFavoritesByUserID(this.id).subscribe(
+        this.recipeFavoriteData.getFavoritesByUserID(this.user.id).subscribe(
           (data: JoinedRF[]) => this.favRecipes = data,
           error => console.error(error)
         );
-
       },
       error => console.error(error)
     );
   }
 
 
-  hideFav() {
-    this.myVar = this.addFav = false;
-  }
-  myVar: any;
-
   //----------------------EDIT-------------------------------------
 
   showFoodImage: string = "";
-
   showUpdateForm: boolean = false;
-
   updateForm() {
     if (this.showUpdateForm === false) {
       this.showUpdateForm = true;
-      
     }
     else {
       this.showUpdateForm = false;
     }
   }
-
   updateRecipeByID() {
     this.recipeData.editRecipe(this.recipe).subscribe(
       data => { this.loadPage(); console.log(data)},
       error => console.error(error)
     );
     this.showUpdateForm = false;
-
   }
 
 
